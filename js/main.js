82c899e55d22f5c426d58e128b0872185a615b68
nailuJ <78428426+nailuJ-dev@users.noreply.github.com>
2021-12-21 18:05:03 +0100

    main js added and html index reorganized with filters

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 // Import modules
+import MainBuilder from "./page/mainBuilder.js";
+import SearchingMessageBuilder from "./page/searchingMessage.js";
+import SearchAlgorithm from "./search/firstAlgo.js";
+import Utils from "./utilities/utils.js";
 
 // Default builder without searching
+MainBuilder.init();
 
 // Builder with searching
-
-// Reset Builder
+document.getElementById('searchInput').addEventListener('keyup', (key) => {
+    let searchValue = key.target.value;
+    if (Utils.characterValid(searchValue)) {
+        let result = SearchAlgorithm.searchingInput(searchValue);
+        if (result.recipesMatch.length === 0) {
+            return SearchingMessageBuilder.noResultMessageBuilder();
+        };
+        Utils.recipeSectionCleared();
+        MainBuilder.initSearch(result);
+        return;
+    };
+    // Reset Builder
+    Utils.recipeSectionCleared();
+    MainBuilder.init();
+});
